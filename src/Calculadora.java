88396500fad9822fe43a89a58ee5b7673b4e6fcd
7ghato <unailopez.yf@gmail.com>
2025-05-29 13:19:38 +0200

    Clase Calculadora agregada

---
+++ src/Calculadora.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author axiel7
+ */
+public class Calculadora {
+    
+    public int suma (int a, int b){
+        return a+b;
+    }
+}
